4069c5ed7616723da7966508a87c008342c48897
davidCañedo <57066973+DavidCV61@users.noreply.github.com>
2022-11-15 10:39:23 -0600

    Create 1743_A.cpp

---
+++ Combinatorics/1743_A.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+ 
+using namespace std;
+ 
+ 
+ 
+int main(){
+ 
+	int t;
+	cin>>t;
+	while(t--){
+		int n;
+	cin>>n;
+	int k=10-n;
+	vector <int> v(n);
+	for(int i=0;i<n;i++)cin>>v[i];
+ 
+		cout<<k*(k-1)*3<<endl;
+	}
+	
+	
+	
+	
+ 
+ 
+ 
+ 
+ 
+   
+ 
+    return 0;
+}
+  
